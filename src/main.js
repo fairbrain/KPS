950322af2e361b38bb8da34e73a5bce9cabdc7f6
Manjur-WD <mak985812@gmail.com>
2025-07-19 01:58:26 +0530

    updated

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-// const videoUrls = [
-//     './images/videos/atta.mp4',
-//     './images/videos/daliya.mp4',
-//     './images/videos/maida.mp4',
-//     './images/videos/rice 2.mp4',
-//     './images/videos/rice.mp4',
-//     './images/videos/suji.mp4',
-// ];
-
-
 window.addEventListener("load", () => {
     const preloader = document.getElementById("preloader");
     setTimeout(() => {
@@ -131,15 +121,6 @@ swiper = new Swiper(".hero-circle .mySwiper", {
         slidePrevTransitionStart: function () {
             currentRotation -= 90;
             rotateDisk();
-        },
-        slideChange: function () {
-            const currentIndex = swiper.realIndex; // use realIndex to ignore duplicated slides in loop mode
-            const video = document.getElementById('hero-video');
-            if (videoUrls[currentIndex]) {
-                video.src = videoUrls[currentIndex];
-                video.load(); // reload the video
-                video.play(); // ensure it starts playing
-            }
         }
     },
     breakpoints: {
